69985ab9af2b5be76c1d40456d0d86e883db07d6
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2017-01-28 10:06:11 +0100

    Upgrade Webpack to 2.x
    * use command line option for dev/build
    * use `modules=false` option for `.babelrc` to enable webpack tree shaking
    * add some more plugins for optimization

--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,59 @@ var path = require('path');
 var readFileSync = require('fs').readFileSync;
 var libraryName = JSON.parse(readFileSync(__dirname + '/package.json')).name;
 
-var plugins = [], outputFile;
 
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-var env = process.env.WEBPACK_ENV;
+module.exports = function(options) {
+  var plugins = [], outputFile;
 
+  if (options === 'build') {
+    plugins = plugins.concat([
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        compress: {
+          warnings: false,
+          'screw_ie8': true
+        },
+        output: {
+          comments: false
+        },
+        sourceMap: false
+      }),
+      new webpack.optimize.OccurrenceOrderPlugin()
+    ]);
+    outputFile = libraryName + '.min.js';
+  } else {
+    outputFile = libraryName + '.js';
+  }
 
-if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = libraryName + '.min.js';
-} else {
-  outputFile = libraryName + '.js';
+  return {
+    entry: __dirname + '/src/index.js',
+    devtool: 'source-map',
+    output: {
+      path: __dirname + '/lib',
+      filename: outputFile,
+      library: libraryName,
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    module: {
+      rules: [
+        {
+          test: /(\.jsx|\.js)$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /(\.jsx|\.js)$/,
+          loader: 'eslint-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      extensions: ['.js']
+    },
+    plugins: plugins
+  };
 }
-
-var config = {
-  entry: __dirname + '/src/index.js',
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/lib',
-    filename: outputFile,
-    library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    loaders: [
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel',
-        exclude: /node_modules/
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: "eslint-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    root: path.resolve('./src'),
-    extensions: ['', '.js']
-  },
-  plugins: plugins
-};
-
-module.exports = config;
