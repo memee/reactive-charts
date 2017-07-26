1c705b03b6820f846ac9150188df8cb5089aab19
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2017-07-26 19:47:16 +0200

    Install jsverify with example

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,9 +1,19 @@
 import test from 'tape';
+import jsc from 'jsverify';
 
 const sth = sth => sth;
 
-test('noop', function(t){
+test('sth', function (t) {
 
   t.equal(sth('something'), 'something');
   t.end();
+});
+
+test('sth', (t) => {
+  t.plan(1);
+  const additionIsCommunative = jsc.checkForall(
+    jsc.integer, jsc.integer, (a, b) => a + b === b + a
+  );
+
+  t.equal(additionIsCommunative, true);
 });
