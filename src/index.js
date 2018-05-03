60e1f880045dc59a243aadb3547672b8e46372f1
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2018-05-03 16:45:58 +0200

    Play with most and cancelable requests

--- src/index.js
+++ src/index.js
@@ -1,22 +1,64 @@
+
+import * as d3 from 'd3';
+import * as most from 'most';
 // @flow
 
-import { chain, compose } from 'ramda';
+import { chain, compose, pathOr } from 'ramda';
+
 import { d3select, append } from './d3io/d3io';
+import { xhrStream } from './network/xhrStream';
 
 import type { IComponent, ILayout, IData, IFunctor } from './types';
 
 
 const renderSVG = compose(chain(append('svg')), d3select);
 
+const POLL_INTERVAL = 1000;
+const API_URL = 'https://sleepy-lake-61171.herokuapp.com/multibar-line';
+
+const retry = (n, stream) =>
+  most.recoverWith(
+    e => n <= 0 ? most.throwError(e) : retry(n - 1, stream), stream
+  );
+
+const getJSON = (url) => xhrStream(() => {
+  const xhr = new XMLHttpRequest();
+  /* eslint-disable fp/no-unused-expression, fp/no-mutation */
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+  /* eslint-enable */
+  return xhr;
+}).map(pathOr({}, ['target', 'response']));
+
+const getNext = (url, interval) =>
+  getJSON(url)
+    .delay(interval)
+    .continueWith(() => getNext(url, interval)); // recursion
+
+// 'http://localhost:3000/multibar-line'
+//
+
+// eslint-disable-next-line no-console
+const consume = ([cost]) => console.log(cost.values[0]);
+
+/* eslint-disable */
+getNext(API_URL, POLL_INTERVAL)
+  .take(5)
+  .observe(consume)
+  .catch(console.error);
+
+
 // FIXME
 /* eslint-disable */
 export const chart = (
-    components: IComponent[],
-    layout: ILayout,
-    dataOrOptions: IData
-    // $FlowFixMe
+  components: IComponent[],
+  layout: ILayout,
+  dataOrOptions: IData
+  // $FlowFixMe
 ): IFunctor<any> => {
-
+  // set the ranges
+  const x = d3.scaleTime().range([0, 500]);
+  const y = d3.scaleLinear().range([300, 0]);
 }
 
 // A `render` function should return a function that'll take chart function as an
